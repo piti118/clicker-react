1e8c03e9a4389a759ac414253c37e747dd492a21
Piti <piti118@gmail.com>
2017-06-04 04:02:17 +0700

    polish home icon

--- src/components/HomeButton.js
+++ src/components/HomeButton.js
@@ -1,17 +1,20 @@
 import React from 'react'
 import HomeIcon from 'material-ui/svg-icons/action/home';
 import {withRouter} from 'react-router'
-import FlatButton from 'material-ui/FlatButton';
+import IconButton from 'material-ui/IconButton';
 
 function HomeButton(props) {
   return (
     <div className="home-button-container">
       <div className="home-button">
-        <FlatButton
-          icon={<HomeIcon color="rgba(0, 0, 0, 0.63)"/>}
+        <IconButton
+          backgroundColor="rgba(0,0,0,0)"
           onClick={()=>{
             props.history.push('/')
-          }}/>
+          }}
+          hoveredStyle={{backgroundColor:'rgba(196, 196, 196, 0.64)'}}>
+          <HomeIcon color="rgba(0, 0, 0, 0.63)"/>
+        </IconButton>
       </div>
     </div>
   )
