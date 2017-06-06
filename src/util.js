c2e7a91922a50f3115374aa32e40e7a00c41b03f
Piti <piti118@gmail.com>
2017-06-06 17:30:25 +0700

    change color

--- src/util.js
+++ src/util.js
@@ -2,9 +2,10 @@ import Color from 'color'
 
 const colorWheel = [
   Color('rgba(255, 111, 111, 0.7)'),
+  Color('rgba(195, 52, 213, 0.7)'),
   Color('rgba(91, 199, 112, 0.7)'),
   Color('rgba(150, 146, 217, 0.7)'),
-  Color('rgba(224, 217, 149, 0.7)')]
+  ]
 
 export function colorObjectForAnswer(a) {
   return colorWheel[(a - 1) % colorWheel.length]
